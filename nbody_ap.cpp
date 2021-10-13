d45f1c310e56202fc252a75ab013e0e655ca628d
Ian <ithomas3@gulls.salisbury.edu>
2021-10-13 13:14:57 -0400

    seq

--- nbody_ap.cpp
+++ nbody_ap.cpp
@@ -11,9 +11,9 @@ const double G = 6.673e-11;
 const double M = 1.0;
 
 struct body {
-    double mass;
-    vector<double> position;
-    vector<double> velocity;
+    double mass = M;
+    double position[2];
+    double velocity[2] = {0, 0};
 };
 
 void initBodies(vector<body> &bodies);
@@ -25,7 +25,7 @@ void v_divide(vector<double> &v, double s);
 int main(int argc, char *argv[]){
 
     int n = atoi(argv[1]);
-    int k = 4;
+    int k = 6;
 
     auto start = chrono::high_resolution_clock::now();
 
@@ -61,7 +61,10 @@ int main(int argc, char *argv[]){
 
     auto stop = chrono::high_resolution_clock::now();
     auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
-    double p = ((k * (n * n))*1.0) / duration.count();
+
+    double time = duration.count() / 10000000.0;
+    
+    double p = ((k * (n * n))*1.0) / time;
     cout << n << ", " << p << endl;
 
     return 0;
@@ -112,10 +115,10 @@ void initBodies(vector<body> &bodies){
     double spacing = 1.0 / bodies.size();
     for(int i=0; i < bodies.size(); i++){
         double y_offset = rand() * 1.0;
-        bodies[i].position.push_back(i * spacing);
-        bodies[i].position.push_back(y_offset);
-        bodies[i].velocity.push_back(0);
-        bodies[i].velocity.push_back(0);
+        bodies[i].position[0] = (i * spacing);
+        bodies[i].position[1] = (y_offset);
+        bodies[i].velocity[0] = 0;
+        bodies[i].velocity[1] = 0;
         bodies[i].mass = M;
     }
 }
--- run_hp.sh
+++ run_hp.sh
@@ -7,17 +7,17 @@ g++ -o nbody nbody_hp.o
 n=( 10 20 50 100 200 500 1000 2000 5000 10000 )
 #n=( 5 10 20 50 )
 
-echo "Form: N, Time(Microseconds)" >> hp.txt
+echo "Form: N, Time(Microseconds)" >> hp.csv
 for i in "${n[@]}"
 do
     echo "Running size $i" 
-    if [ $i -lt 2001 ]
-    then
-        for j in {1..5}
-        do
-            ./nbody "$i" >> hp.txt
-        done
-    else
-       ./nbody "$i" >> hp.txt
-    fi
+    #if [ $i -lt 2001 ]
+    #then
+    #    for j in {1..5}
+    #    do
+    #        ./nbody "$i" >> hp.txt
+    #    done
+    #else
+       ./nbody "$i" >> hp.csv
+    #fi
 done
